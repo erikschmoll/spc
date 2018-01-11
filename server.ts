fd9f97ceb2ee9d451a16fdc6a9fa20fb549f9221
Erik Schmoll <erikschmoll@gmail.com>
2018-01-11 14:25:43 -0300

    subo config para el tsc

--- server.ts
+++ server.ts
@@ -6,13 +6,9 @@ import { MongoClient } from 'mongodb'
 //connect to mongodb
 
 //var uri = "mongodb://spc:sofrecom10!@cluster0-shard-00-00-uyim5.mongodb.net:27017/dev";
-var uri = "mongodb://spc:sofrecom10!@cluster0-shard-00-00-uyim5.mongodb.net:27017,cluster0-shard-00-01-uyim5.mongodb.net:27017,cluster0-shard-00-02-uyim5.mongodb.net:27017/dev?ssl=true";
+var uri = "mongodb://spc:sofrecom10!@cluster0-shard-00-00-uyim5.mongodb.net:27017,cluster0-shard-00-01-uyim5.mongodb.net:27017,cluster0-shard-00-02-uyim5.mongodb.net:27017/dev";
 
-var options = {
-    "ssl": true,
-    "sslValidate": false
-  }
-MongoClient.connect(uri, options, function(err, db) {
+MongoClient.connect(uri, function(err, db) {
   db.close();
 });
 
