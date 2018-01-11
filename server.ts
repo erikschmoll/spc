4512d2bde6b74732a2ad1f4db87e588bf0cea46c
Edward Schmoll <eschmoll@sofrecom.com.ar>
2018-01-11 12:21:04 -0300

    subo la conexión a mongo atlas

--- server.ts
+++ server.ts
@@ -1,8 +1,21 @@
 import { config } from './config/env'
 import { app } from './config/restify'
+import { MongoClient } from 'mongodb'
 
 
 //connect to mongodb
+
+//var uri = "mongodb://spc:sofrecom10!@cluster0-shard-00-00-uyim5.mongodb.net:27017/dev";
+var uri = "mongodb://spc:sofrecom10!@cluster0-shard-00-00-uyim5.mongodb.net:27017,cluster0-shard-00-01-uyim5.mongodb.net:27017,cluster0-shard-00-02-uyim5.mongodb.net:27017/dev?ssl=true";
+
+var options = {
+    "ssl": true,
+    "sslValidate": false
+  }
+MongoClient.connect(uri, options, function(err, db) {
+  db.close();
+});
+
 
 
 app.listen(config.port,()=>{
