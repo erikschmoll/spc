476cfbc841c2be229eda0770e97fd4e0f3fe761a
Edward Schmoll <eschmoll@sofrecom.com.ar>
2018-01-29 16:15:27 -0300

    agrego el componente edge para leer dlls

---
+++ app/services/call.dll.service.ts
@@ -0,0 +1,19 @@
+
+var edge = require('edge')
+
+
+class CallDllService{
+    constructor(){
+
+    }
+
+    call = (dll:string, payload:any, cb: any):void => 
+    {
+        
+    }
+
+}
+
+
+export { CallDllService }
+
